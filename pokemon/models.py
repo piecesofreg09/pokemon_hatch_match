2a2d0cb94c132cbf8ec96593625f822e905ac7d3
Lu, Hongfang <hlu82@gatech.edu>
2020-08-05 11:05:48 -0400

    Update models.py

--- pokemon/models.py
+++ pokemon/models.py
@@ -8,28 +8,74 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Pokemon(models.Model):
-    """Model representing a book genre."""
-    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+    """Model representing a pokemon."""
+    idd = models.IntegerField(primary_key=True, help_text='Pokemon id in the official game')
+    name = models.CharField(max_length=20, help_text='Name of the pokemon')
+
+    weight = models.IntegerField()
+
+    stat = models.OneToOneField(Stat)
+    generation = models.ForeignKey(Generation)
+    types = models.ManyToManyField(Type)
     
     def __str__(self):
         """String for representing the Model object."""
         return self.name
+    
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
 
 
 class Generation(models.Model):
-    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
-    name = models.CharField(max_length=200,
-                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+    """Model representing the generation number, such as 1, 2, 3, ..."""
+    generation_number = models.IntegerField(primary_key=True, 
+        help_text='the official generation number')
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.generation_number
+    
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('generation-detail', args=[str(self.generation_number)])
+        
+class Type(models.Model):
+    """Model representing the type, such as grass, poison, ..."""
+    name = models.CharField(max_length=30,
+                            help_text="the type of the selected pokemon")
 
     def __str__(self):
         """String for representing the Model object (in Admin site etc.)"""
         return self.name
-        
-class Type(models.Model):
-    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
-    name = models.CharField(max_length=200,
-                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+    
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('type-detail', args=[str(self.id)])
+    
+class Stat(models.Model):
+    """Stat object, including six stats:
+    hp, attack, defense, special-attack, special-defense, speed"""
+
+    hp = models.IntegerField(help_text='Hit point (health point)')
+    attack = models.IntegerField(help_text='Attack point')
+    defense = models.IntegerField(help_text='Ability to defend, mapped to a ratio')
+    special_attack = models.IntegerField(help_text='Special attack point')
+    special_attack_acc_rounds = models.IntegerField(help_text='Rounds to trigger special attack')
+    special_defense = models.IntegerField(help_text='Special defense point')
+    special_defense_probability = models.FloatField(
+        help_text='Probability to trigger special defense')
+    speed = models.IntegerField(help_text='Speed')
 
     def __str__(self):
         """String for representing the Model object (in Admin site etc.)"""
-        return self.names
+        return f'{self.hp}, {self.attack}, {self.defense}'
+
+class Sprites(models.Model):
+    """Sprites links for a specific pokemon"""
+    back_default = models.URLField(max=300, verbose_name='default back url')
+    front_default = models.URLField(max=300, verbose_name='default back url')
+    big_sprite = models.URLField(max=300, verbose_name='default back url')
+
+    def __str__(self):
+        return 'Sprites object'
